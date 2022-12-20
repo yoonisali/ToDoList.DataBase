752407b67231ce75f5f11af66d221f9d2f4abafc
yoonisali <yoonismahamoudali@gmail.com>
2022-12-20 11:13:58 -0800

    initial commit
    co-authored-by: Robert Bryan <robbryandev@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,28 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public class ItemsController : Controller
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
-    }
+        private readonly ToDoListContext _db;
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
-      return View(model);
+        public ItemsController(ToDoListContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Item> model = _db.Items.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Item thisItem = _db.Items.FirstOrDefault(item => item.Id == id);
+            return View(thisItem);
+        }
+
+
+
+
+
     }
-  }
 }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,66 +1,39 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoList.Models;
 
 namespace ToDoList
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-      builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews();
 
-      DBConfiguration.ConnectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+            builder.Services.AddDbContext<ToDoListContext>(
+              dbContextOptions => dbContextOptions
+                .UseMySql(
+                  builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                )
+              )
+            );
 
-      WebApplication app = builder.Build();
+            WebApplication app = builder.Build();
 
-    //   app.UseDeveloperExceptionPage();
-      app.UseHttpsRedirection();
-      app.UseStaticFiles();
+            // app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-      app.UseRouting();
+            app.UseRouting();
 
-      app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
-      app.Run();
+            app.Run();
+        }
     }
-  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// using System.IO;
-// using Microsoft.AspNetCore.Hosting;
-
-// namespace ToDoList
-// {
-//   public class Program
-//   {
-//     public static void Main(string[] args)
-//     {
-//       var host = new WebHostBuilder()
-//         .UseKestrel()
-//         .UseContentRoot(Directory.GetCurrentDirectory())
-//         .UseIISIntegration()
-//         .UseStartup<Startup>()
-//         .Build();
-
-//       host.Run();
-//     }
-//   }
-// }
